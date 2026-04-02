8c030ece1a7e86f5bba5af70ae01049c86aacd6d
SIVATEJA2005 <157879004+SIVATEJA2005@users.noreply.github.com>
2026-04-03 00:17:17 +0530

    adding after chaging category controllers

--- src/main/java/com/project/moneymanager/controllers/CatogoryControllers.java
+++ src/main/java/com/project/moneymanager/controllers/CatogoryControllers.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 import java.util.Map;
 @RestController
-@RequestMapping("/api/v1.0/category")
+@RequestMapping("/api/v1.0/categories")
 public class CatogoryControllers
 {
     @Autowired
--- src/main/java/com/project/moneymanager/controllers/ProfileControllers.java
+++ src/main/java/com/project/moneymanager/controllers/ProfileControllers.java
@@ -7,10 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.security.core.Authentication;
-
 import java.util.Map;
 @RestController
 @RequestMapping("/api/v1.0")
